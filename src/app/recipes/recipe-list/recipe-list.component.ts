72128cea9f9d4c391399aec6ba4426c09b62c193
Beto <jrs_robert@hotmail.com>
2018-11-03 20:20:48 -0300

    Display recipes from firebase

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe/recipe.model';
 import { Step } from '../recipe/step.model';
 import { Ingredient } from '../recipe/ingredient.model';
+import { Server } from '../../../providers/server.service';
+import { map } from 'rxjs/operators';
+import { Response } from '@angular/http';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,14 +14,23 @@ import { Ingredient } from '../recipe/ingredient.model';
 export class RecipeListComponent implements OnInit {
   steps: Step[] = [new Step('Misturar a carne', 'Lorem ipsum dot color')];
   ingredients: Ingredient[] = [new Ingredient('Cominho', 1)];
-  recipes: Recipe[] = [new Recipe( 'Strongonofe de Frango Simples', 'Strongofe de frango feito com creme de leite e molho de tomate que fica uma delícia', this.steps, this.ingredients )];
+  recipes: Recipe[] = [new Recipe( 'Strongonofe de Frango Simples', 'Strongofe de frango feito com creme de leite e molho de tomate que fica uma delícia')];
   recipe: Recipe;
-  constructor() {
-    this.recipe = new Recipe( 'Strongonofe de Frango Simples', 'Strongofe de frango feito com creme de leite e molho de tomate que fica uma delícia', this.steps, this.ingredients );
+  constructor(private server: Server) {
+    this.recipe = new Recipe( 'Strongonofe de Frango Simples', 'Strongofe de frango feito com creme de leite e molho de tomate que fica uma delícia');
   }
 
   ngOnInit() {
-
+    this.server.getRecipes()
+      .pipe( 
+        map( (res: Response) => res.json() ) 
+      )
+      .subscribe( 
+        data => {
+          for ( var [k, v] of Object.entries( data ) ) {
+            this.recipes.push( new Recipe(v['title'], v['description']) )
+          }
+        } );
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Recipe } from './recipe/recipe.model';
+import { Server } from '../../providers/server.service';
 
 @Component({
   selector: 'app-recipes',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formElement') formElement: ElementRef;
+  @ViewChild('titleElement') titleElement: ElementRef;
+  @ViewChild('desclement') descElement: ElementRef;
+  constructor(private server: Server) { }
 
   ngOnInit() {
   }
+
+  submitNewRecipeData( event, title, description ){
+    event.preventDefault();
+    this.addNewRecipe( title, description );
+    
+  }
+
+  addNewRecipe( title, description ){
+    this.server
+        .createRecipe( new Recipe(title, description) )
+        .subscribe(
+          res => console.log( res ),
+          err => console.log( err ),
+          () => console.log( 'done' )
+        );
+  }
+
+  
 
 }
