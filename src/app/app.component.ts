91e4d084a6cad2f65d83110be55cfca9c5f38484
Beto <jrs_robert@hotmail.com>
2018-11-12 22:54:51 -0200

    Improve animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,22 +12,24 @@ import { ActivatedRoute } from '@angular/router';
         style({
           height: '!'
         }),
-        query(':enter', style({ transform: 'translateX(100%)' })),
+        query(':enter', style({ transform: 'translateY(50%)', zIndex: '2', opacity: '1' })),
         query(':enter, :leave', style({ position: 'absolute', top: '0', right: '0', left: '0', bottom: '0' })),
         group([
-          query(':leave', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateX(-100%)'}))]),
-          query(':enter', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateX(0)'}))]),
+          query(':enter', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateY(0%)', background: '#fff'}))]),
+          //query(':leave', [ animate('420ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateY(100%)'}))]),
+          query(':leave', [ animate('880ms ease-in', style({ opacity: '0'}))]),
         ])
       ] ),
       transition( '2 => 1', [
         style({
           height: '!'
         }),
-        query(':enter', style({ transform: 'translateX(-100%)' })),
+        query(':enter', style({ transform: 'translateY(0)', opacity: '0' })),
         query(':enter, :leave', style({ position: 'absolute', top: '0', right: '0', left: '0', bottom: '0' })),
         group([
-          query(':leave', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateX(100%)'}))]),
-          query(':enter', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateX(0)'}))]),
+          query(':leave', [ animate('620ms ease-in', style({transform: 'translateY(100%)'}))]),
+          //query(':enter', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateY(0)'}))]),
+          query(':enter', [ animate('880ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({opacity: '1'}))]),
         ])
       ] )
     ])
