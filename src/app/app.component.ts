58e57f85eb824cf70a990955c6e817e0e7ca7127
Beto <jrs_robert@hotmail.com>
2018-11-12 22:57:14 -0200

    Adjust animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,9 +27,9 @@ import { ActivatedRoute } from '@angular/router';
         query(':enter', style({ transform: 'translateY(0)', opacity: '0' })),
         query(':enter, :leave', style({ position: 'absolute', top: '0', right: '0', left: '0', bottom: '0' })),
         group([
-          query(':leave', [ animate('620ms ease-in', style({transform: 'translateY(100%)'}))]),
+          query(':leave', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateY(100%)'}))]),
           //query(':enter', [ animate('620ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({transform: 'translateY(0)'}))]),
-          query(':enter', [ animate('880ms cubic-bezier(0.870, 0.000, 0.560, 0.990)', style({opacity: '1'}))]),
+          query(':enter', [ animate('880ms ease-in', style({opacity: '1'}))]),
         ])
       ] )
     ])
